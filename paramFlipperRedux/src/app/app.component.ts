4901690128678de6decf81a908ac771c6c1d3db7
mharmony <mharmon@apogeeintegration.com>
2022-05-18 15:50:47 -0400

    New project...

---
+++ paramFlipperRedux/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { AfterViewInit, Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DataSource } from '@tableau/extensions-api-types';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements AfterViewInit {
+  public title = 'Param Flipper';
+  public currentlyPlaying = false;
+  public currentParamValue: any;
+
+  public paramForm = new FormGroup({
+    parameter: new FormControl('', [Validators.required]),
+    flipDuration: new FormControl(2.0, [Validators.required]),
+    stepSize: new FormControl(''),
+    stepSizeUnit: new FormControl(''),
+    minVal: new FormControl(''),
+    maxVal: new FormControl(''),
+    loop: new FormControl(false)
+  });
+
+  public async ngAfterViewInit() {
+    await tableau.extensions.initializeAsync();
+  }
+
+  public async findParameter(paramName: string) {
+    const params = await tableau.extensions.dashboardContent?.dashboard.getParametersAsync();
+
+    return params?.find((x) => x.name === paramName);
+  }
+
+  public startStopPlayback() {
+    this.currentlyPlaying = !this.currentlyPlaying;
+  }
+
+  private async getParameters() {
+    return await tableau.extensions.dashboardContent?.dashboard.getParametersAsync();
+  }
+}
